08dc0bd5281ac414fae0fa7fa559cd4c570bbd14
kevinpicon01 <digitalpruu@gmail.com>
2024-10-03 18:45:58 -0500

    Fix

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -1,36 +1,38 @@
-const title = "Titulo del emprendimiento";
-const email = "Email del emprendimiento";
+const title = "RoGa";
+const slogan = "Ropa para toda la familia";
+const email = "wendyl-2114@hotmail.com";
 const mensajeWhatsApp = encodeURIComponent(
-    `¡Hola! Estoy muy interesado en tus productos de ${title} y me gustaría recibir más información sobre ellos. Espero poder hablar pronto contigo para aclarar algunas dudas. ¡Gracias!`
+    `¡Hola! Estoy interesado en las prendas de vestir de ${title} y me gustaría obtener más información. ¡Gracias!`
 );
-const numeroWhatsApp = "Numero del emprendimiento";
+const numeroWhatsApp = "3102228783";
+
 const textos = {
     index: {
-        title: title,
+        title: `${title}`,
+        slogan: `${slogan}`,
     },
     homeContent: {
-        title: title,
+        title: `${title}`,
     },
     about: {
-        description1: "Descripción del emprendimiento parrafo uno",
-        description2: "Descripcion del emprendimiento parrafo dos"
+        description1: "RoGa es tu tienda de confianza para prendas de vestir que se adaptan a toda la familia. Nos especializamos en ofrecer ropa de calidad que combina estilo y comodidad, asegurándonos de que cada miembro de tu familia encuentre algo que le encante. En RoGa, creemos que la moda debe ser accesible y versátil, por eso ofrecemos una amplia gama de prendas que se ajustan a diferentes estilos y ocasiones."
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
-        email: email
+        email: email,
     },
     footer: {
-        slogan:{
-            p1: "slogan del emprendimiento parrafo uno",
-            p2: "slogan del emprendimiento parrafo dos"
+        slogan: {
+            p1: "",
+            p2: slogan,
         },
-        address: "Dirección del emprendimiento",
+        address: "Dirección del negocio",
         phone: numeroWhatsApp,
         email: email,
-        facebook: "https://www.facebook.com/",
+        facebook: "https://www.facebook.com/share/1MJZ7r8n69zW8Dzf/?mibextid=qi2Omg",
         instagram: "https://www.instagram.com/",
-        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`
+        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
     },
-
 };
+
 export default textos;
